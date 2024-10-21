826bd7b9d4e678bf7575893a3984149ef99f25c8
Ratul-Raha <ratulraha.04@gmail.com>
2024-10-21 18:16:48 +0600

    It is initial massage

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Demo from './Components/Demo';
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World ! I am app page</h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Demo/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Demo.jsx
@@ -0,0 +1,11 @@
+
+
+const Demo = () => {
+    return (
+        <div>
+             <h1>Hello i am demo Html</h1>
+        </div>
+    );
+};
+
+export default Demo;
